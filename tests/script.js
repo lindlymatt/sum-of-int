6f93f09a4ab43fcc009519cf2c15ce20c60d27e7
Matthew Lindly <matthew.lindly@hotmail.com>
2017-01-05 09:46:28 -0700

    Testing finished.

--- tests/script.js
+++ tests/script.js
@@ -2,17 +2,14 @@
 
 const code = require('../script.js');
 var it = require('mocha');
-var insert = require('chai').insert;
 var expect = require('chai').expect;
 
 describe('Two Sum', () => {
 
-  // Will handle expected response.
   it('Should return the indices of two values that total the target integer.', () => {
     expect(code.twoSum([1, 3, 5, 7, 9], 8)).to.equal([0, 3]);
   });
 
-  // Will handle indicated type.
   it('Should return an array of indices.', () => {
     expect(code.twoSum([1, 3, 5, 7, 9])).to.be.a('array');
   });
